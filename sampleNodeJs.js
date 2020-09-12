eaef755b35d870d85ca81524f14887804b90e430
kris2727 <kreddiva@uncc.edu>
2020-09-12 09:16:12 -0400

    Creating a express server

--- sampleNodeJs.js
+++ sampleNodeJs.js
@@ -1,4 +1,6 @@
 console.log("Hello World & System Integration Class");
+
+//creating a simple server
 const http = require('http');
 const server = http.createServer((req,res)=>{
     res.write("Hello World & System Integration Class");
@@ -6,3 +8,16 @@ const server = http.createServer((req,res)=>{
 });
 
 server.listen(9000);
+
+//creating a node JS server using express
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World & System Integration Class from Express server')
+})
+
+app.listen(4000, () => {
+  console.log('Express port listening at port 4000');
+})
