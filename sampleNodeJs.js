e36d156c4699e782705b7f6c540fda29398a8c5d
kris2727 <kreddiva@uncc.edu>
2020-09-12 08:40:11 -0400

    Creating a simple Node Js server

--- sampleNodeJs.js
+++ sampleNodeJs.js
@@ -1,1 +1,8 @@
 console.log("Hello World & System Integration Class");
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    res.write("Hello World & System Integration Class");
+    res.end();
+});
+
+server.listen(9000);
